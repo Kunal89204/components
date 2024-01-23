19b77a3ad72cb64a1f3dbfd97736aba4fe01d0a8
Kunal89204 <Kunalkhandelwal108@gmail.com>
2024-01-23 16:15:31 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './components/Title';
+
+function App() {
+  return (
+    
+    <Title />
+    
+  );
+}
+
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <div className='text-2xl text-blue-300'>
+      This is a title page testiung again again again
+    </div>
+  )
+}
