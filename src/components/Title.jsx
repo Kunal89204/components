1c2391a426ab0c7b9018414249622b2721105e47
Kunal89204 <Kunalkhandelwal108@gmail.com>
2024-01-23 16:55:26 +0530

    nav

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Title() {
   return (
     <div className='text-2xl text-blue-300'>
-      This is a title page testiung again again again
+      <nca>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Shop</li>
+          <li>Contact Us</li>
+        </ul>
+      </nca>
     </div>
   )
 }
